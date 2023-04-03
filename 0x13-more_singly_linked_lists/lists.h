5a6d43dc6963ac82746846edab8dc812e8961b52
Mikail Rokib <atandarokib@gmail.com>
2023-04-03 14:14:20 -0700

    Readme Data Structure Updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,19 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
- * Des: singly linked list node structure
- * for Holberton project
+ * Description: singly linked list node structure
+ *
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 /**
